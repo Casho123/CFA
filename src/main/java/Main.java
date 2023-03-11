445af79afc060749d0cddb5d6b93f69f0be8d799
My PC <alex.petkov606@gmail.com>
2023-03-11 19:29:15 +0200

    asd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import java.math.BigDecimal;
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.util.Date;
 
 public class Main {
@@ -19,10 +18,11 @@ public class Main {
         sale.setDate(Date.from(Instant.now()));
 
         Product product = new Product();
-        product.setName("fds");
+        product.setName("fdsdagdsasfass");
         product.setPrice(BigDecimal.ONE);
         product.setQuantity(10);
         product.getSales().add(sale);
+        sale.setProduct(product);
 
         entityManager.persist(product);
 
