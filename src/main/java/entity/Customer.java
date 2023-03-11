81a40091d2f083228b2471ff4c8d6f260930fe03
My PC <alex.petkov606@gmail.com>
2023-03-11 18:40:02 +0200

    asd

---
+++ src/main/java/entity/Customer.java
@@ -0,0 +1,42 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class Customer extends BaseEntity {
+
+    private String name;
+    private String email;
+    private String creditCardNumber;
+
+    public Customer() {
+    }
+
+    @Column(nullable = false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Column(nullable = false, unique = true)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    @Column(name = "credit_card_number")
+    public String getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(String creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+}
